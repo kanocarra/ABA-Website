f5ef1633ca2fc8278d2cdff5f909191c3beeae5f
kanocarra <emily.melhuish@gmail.com>
2015-08-30 22:46:37 +1200

    Google Maps on page - not right position

--- js/GoogleMaps.js
+++ js/GoogleMaps.js
@@ -2,3 +2,10 @@
  * Created by Emily on 29/08/2015.
  */
 // API KEY: AIzaSyD0F_2kiAs1YGgtBjaj7FM1cJYq6d38Gb4
+var map;
+function initialiseMap() {
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+        center: {lat: -36.8630231, lng: 174.8654693},
+        zoom: 12
+    });
+}
