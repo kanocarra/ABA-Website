908e46e4c2ed8ae4c744677acd176746af4bb77e
kanocarra <emily.melhuish@gmail.com>
2015-08-31 19:12:19 +1200

    Maps workign properly

--- js/GoogleMaps.js
+++ js/GoogleMaps.js
@@ -3,9 +3,39 @@
  */
 // API KEY: AIzaSyD0F_2kiAs1YGgtBjaj7FM1cJYq6d38Gb4
 var map;
+var markers = [];
 function initialiseMap() {
     map = new google.maps.Map(document.getElementById('map-canvas'), {
-        center: {lat: -36.8630231, lng: 174.8654693},
+        //center on Auckland
+        center: {lat: -36.8325841, lng: 174.7982726},
         zoom: 12
     });
+    addMarkers();
+}
+
+function drop() {
+    for (var i =0; i < markerArray.length; i++) {
+        setTimeout(function() {
+            addMarker();
+        }, i * 200);
+    }
+}
+
+function addMarker() {
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: {lat: -36.8325841, lng: 174.7982726}
+    });
+    marker.addListener('click', toggleBounce);
+}
+
+
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
 }
