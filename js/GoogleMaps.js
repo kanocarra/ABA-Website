c71ce0cc9ee50c79745ade69b3a6141ed0073095
kanocarra <emily.melhuish@gmail.com>
2016-02-08 16:28:47 +1300

    Events updated

--- js/GoogleMaps.js
+++ js/GoogleMaps.js
@@ -3,49 +3,88 @@
  */
 
 var map;
+var markers = [];
+var marker;
+
+
 var bands = [
     {
         name: "Whangarei District Brass",
         lat: -35.7550607,
         lng: 174.3124363,
-        link: "https://www.facebook.com/whangareidistrictbrass/info?tab=page_info"
+        link: "https://www.facebook.com/whangareidistrictbrass/info?tab=page_info",
+        address: "8A Dyer Street <br> Whangarei <br> Northland <br>"
     },
     {   name: "Kumeu Brass",
         lat: -36.7800327,
         lng: 174.5537317,
-        link: "http://www.kumeubrass.org.nz/index.php"
+        link: "http://www.kumeubrass.org.nz/index.php",
+        address: "Gate 7 Kumeu Showgrounds <br>  Waitakere Road <br> Kumeu <br>"
     },
     {   name: "Waitakere Auckland Brass",
         lat: -36.907943,
         lng: 174.69068,
-        link: "http://www.waitakerebrass.com/"
+        link: "http://www.waitakerebrass.com/",
+        address: "36 Portage Rd <br> New Lynn <br> Auckland <br>"
     },
     {   name: "North Shore Brass",
         lat: -36.7871699,
         lng: 174.7602339,
-        ink: "http://www.northshorebrass.org.nz/"},
+        link: "http://www.northshorebrass.org.nz/",
+        address: "13 Taharoto Road <br> Takapuna <br> Auckland <br> "
+
+    },
+
     {
         name: "North Shore Youth Brass",
         lat: -36.7883307,
         lng: 174.7598231,
-        link: "http://www.northshoreyouthbrassband.org/"
+        link: "http://www.northshoreyouthbrassband.org/",
+        address: "13 Taharoto Road <br> Takapuna <br> Auckland <br> "
     },
     {
         name: "Takapuna Grammar School Brass",
         lat: -36.8015774,
         lng: 174.7903275,
-        link: "http://www.takapuna.school.nz/news/student-achievement/"
+        link: "http://www.takapuna.school.nz/news/student-achievement/",
+        address: "210 Lake Rd <br> Takapuna <br> Auckland <br>"
     },
     {   name: "Auckland City Brass",
         lat: -36.907635,
         lng: 174.757163,
-        link: "http://www.aucklandcitybrass.co.nz/"
-    }
-    //TODO: Add rest of bands in
-    //{"name":"Howick Brass", lat:-36.907635, lng:174.757163, link:"http://www.aucklandcitybrass.co.nz/"},
-    //{"name":"Auckland City Brass", lat:-36.907635, lng:174.757163, link:"http://www.aucklandcitybrass.co.nz/"},
-    //{"name":"Auckland City Brass", lat:-36.907635, lng:174.757163, link:"http://www.aucklandcitybrass.co.nz/"},
-    //{"name":"Auckland City Brass", lat:-36.907635, lng:174.757163, link:"http://www.aucklandcitybrass.co.nz/"},
+        link: "http://www.aucklandcitybrass.co.nz/",
+        address: "Grahame Breed Dr <br> Three Kings <br> Auckland <br>"
+    },
+
+    {
+        "name":"Howick Brass",
+        lat:-36.896983,
+        lng:174.9049237,
+        link:"https://www.facebook.com/howick.brassband",
+        address: "Howick"
+    },
+
+    {
+        "name":"Dalewool Auckland Brass",
+        lat:-36.9269398,
+        lng:174.8010153,
+        link:"http://www.nzbrass.com/",
+        address: "98 Captain Springs Road <br> Te Papapa <br> Auckland<br>"
+    },
+    {
+        "name":"Dalewool Youth Brass",
+        lat:-36.926983,
+        lng:174.8,
+        link:"http://www.dalewoolyouth.com/",
+        address: "98 Captain Springs Road <br> Te Papapa <br> Auckland<br>"
+    },
+    {
+        "name":"Papakura Brass",
+        lat:-37.0554291,
+        lng:174.9421477,
+        link:"https://www.facebook.com/PapakuraBrass/",
+        address:"104 Arimu Road <br> Papakura <br> Auckland <br>"
+    },
 ];
 
 function initialiseMap() {
@@ -55,46 +94,116 @@ function initialiseMap() {
         zoom: 9
     });
 
-   //Delay dropping of markers until map is fully loaded
+    //Delay dropping of markers until map is fully loaded
     setTimeout(function(){
         addMarkers();
     },700);
 
+    loadBands();
 }
 
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+    setAllMap(null);
+}
 
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
 
-var currentBand;
-
-function addMarkers() {
-    var i;
-    var markers = [];
-    var marker;
-
-    for(i=0; i < bands.length; i++) {
-        currentBand = bands[i];
-        marker = new google.maps.Marker({
-            map: map,
-            draggable: false,
-            animation: google.maps.Animation.DROP,
-            position: {lat:currentBand.lat, lng: currentBand.lng},
-            title: "Band"
-        });
-
-        var textContent = "Band: " + currentBand.name;
-
-        var infoWindow = new google.maps.InfoWindow({
-            content: textContent
-        });
-        //TODO: Add info window for all markers
-        google.maps.event.addListener(marker, 'click', function () {
-            infoWindow.open(map, marker);
-        });
-        markers.push(marker);
+function setAllMap(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
     }
 }
 
 
+function addMarkers() {
+    deleteMarkers();
+
+    var image = {
+        url: 'img/cornet_symbol.png',
+        // This marker is 20 pixels wide by 32 pixels high.
+        size: new google.maps.Size(30,50),
+        // The origin for this image is (0, 0).
+        origin: new google.maps.Point(0, 0),
+        // The anchor for this image is the base
+        anchor: new google.maps.Point(0, 50)
+    };
+
+    bands.forEach(function (band) {
+        var position = new google.maps.LatLng(band.lat, band.lng);
+
+        marker = new google.maps.Marker({
+            map: map,
+            draggable: false,
+            animation: google.maps.Animation.DROP,
+            position: position,
+            title: "" + band.name,
+            icon: image
+        });
+        attachMessage(marker, band);
+        markers.push(marker);
+
+
+        var bounds = new google.maps.LatLngBounds();
+        for (i = 0; i < markers.length; i++) {
+            bounds.extend(markers[i].getPosition());
+        }
+
+        map.fitBounds(bounds);
+
+    })
+}
+
+function attachMessage(marker, band) {
+    var infowindow = new google.maps.InfoWindow({
+        content: "<b>" + band.name + "</b><br>" + band.address +  "<br>Visit their <a href = " + band.link + " target = \"_blank\">website. </a>"
+    });
+
+    marker.addListener('click', function() {
+        infowindow.open(marker.get('map'), marker);
+    });
+}
+
+
+function loadBands() {
+    var div = document.getElementById('bands');
+    div.innerHTML += '<ul>';
+    bands.forEach(function(band) {
+        div.innerHTML = div.innerHTML + '<a style = "color: #333333" href = "#"><li style = "list-style: none; margin: 10px;" onClick="centerOnBand(\'' + band.name + '\')">' + band.name + '</li></a>';
+    })
+    div.innerHTML += '</ul>'
 
 
 
+
+}
+
+function centerOnBand(name) {
+    var lat;
+    var lng;
+    console.log(name);
+    bands.forEach(function(band) {
+        if(band.name == name){
+            lat = band.lat;
+            lng = band.lng;
+
+            var infowindow = new google.maps.InfoWindow({
+                content: "<b>" + band.name + "</b><br>" + band.address +  "<br>Visit their <a href = " + band.link + " target = \"_blank\">website. </a>"
+            });
+
+            markers.forEach(function(marker){
+                if(marker.getTitle() == name){
+                    infowindow.open(marker.get('map'), marker);
+                }
+            })
+
+        }
+    })
+
+    map.setCenter(new google.maps.LatLng(lat, lng));
+    map.setZoom(15);
+}
